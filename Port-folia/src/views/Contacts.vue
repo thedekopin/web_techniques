697e10d6959de2801aed5b0606385fcf7955a4ce
e1b14069 <thedekopin0108@gmail.com>
2019-02-24 04:32:10 +0900

    ディレクトリ名の変更

---
+++ Port-folia/src/views/Contacts.vue
@@ -0,0 +1,7 @@
+<template>
+  <div id="contacts_contents">
+    <br>
+    <h1>Contacts</h1>
+    
+  </div>
+</template>
---
+++ Port-folia/src/views/Top.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="top">
+    <br>
+    <HelloWorld msg="Welcome to Port-folia"/>
+    <TopContents></TopContents>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import TopContents from "@/components/TopContents.vue";
+
+export default {
+  name: "top",
+  components: {
+    HelloWorld,
+    TopContents
+  }
+};
+</script>
